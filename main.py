33c81ce3e844d5f40f452c085e80a3fba38f90d0
Fadi Rached <fadirayrached@gmail.com>
2022-10-06 15:16:39 -0400

    Cleanup

--- main.py
+++ main.py
@@ -5,16 +5,33 @@ from selenium.webdriver.common.by import By
 from requests import post
 
 API_KEY = ''
+QUICK_START_BUTTON_XPATH = '//*[@id="root"]/div/div/div[1]/div[3]/div/div/div[1]/div[1]/div/div/div/div/div/div/div/div[1]/div/div/div[2]/div/div/div[4]/div[1]'
+TURN_OFF_EVENTS_BUTTON_XPATH = '/html/body/div[3]/div/div[2]/div/div/div/div[1]/div[2]/div/div/div[2]/div/div[2]'
+SUBMIT_BUTTON_XPATH = '//*[@id="root"]/div/div/div[1]/div[3]/div/div/div[1]/div[1]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[1]/div[2]/div[2]/div[2]'
+TEXT_AREA_XPATH = '//*[@id="root"]/div/div/div[1]/div[3]/div/div/div[1]/div[1]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[1]/div[2]/div[2]/div[1]/textarea'
+STORY_TEXT_DIV_XPATH = '//*[@id="root"]/div/div/div[1]/div[3]/div/div/div[1]/div[1]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[1]/div[1]/div/div'
+AI_DUNGEON_URL = 'https://play.aidungeon.io/main/worldStart?worldPublicId=1542906f-3100-4849-b28b-e6b11a8dd0b8'
+DEEP_AI_URL = 'https://api.deepai.org/api/text2img'
+
+
+def SetupDriver():
+    service = Service(executable_path="chromedriver.exe")
+    options = webdriver.ChromeOptions()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
+
 
 def GetApiKey():
     with open('deep-ai.txt') as file:
         text = file.read()
-        global API_KEY 
+        global API_KEY
         API_KEY = text
+
 
 def GenerateImage(text):
     r = post(
-        "https://api.deepai.org/api/text2img",
+        DEEP_AI_URL,
         data={
             'text': text,
         },
@@ -22,36 +39,36 @@ def GenerateImage(text):
     )
 
     json = r.json()
-    print('\nGenerated Image URL: ' + json['output_url']+ '\n')
+    print('\nGenerated Image URL: ' + json['output_url'] + '\n')
 
 
-def LaunchAIDungeon():
-    service = Service(executable_path="chromedriver.exe")
-    options = webdriver.ChromeOptions()
-    options.add_argument("--log-level=3")
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.get('https://play.aidungeon.io/main/worldStart?worldPublicId=1542906f-3100-4849-b28b-e6b11a8dd0b8')
-    sleep(5)
-    quickstart_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[1]/div[3]/div/div/div[1]/div[1]/div/div/div/div/div/div/div/div[1]/div/div/div[2]/div/div/div[4]/div[1]')
+def LoginToAIDungeon(driver: webdriver.Chrome):
+    driver.get(AI_DUNGEON_URL)
+    sleep(10)
+    quickstart_button = driver.find_element(By.XPATH, QUICK_START_BUTTON_XPATH)
     quickstart_button.click()
     sleep(20)
-    turn_off_events_button =  driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div/div/div/div[1]/div[2]/div/div/div[2]/div/div[2]')
+    turn_off_events_button = driver.find_element(
+        By.XPATH, TURN_OFF_EVENTS_BUTTON_XPATH)
     turn_off_events_button.click()
-    sleep(1)
-    playing = True
-    submit_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[1]/div[3]/div/div/div[1]/div[1]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[1]/div[2]/div[2]/div[2]')
-    text_area = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[1]/div[3]/div/div/div[1]/div[1]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[1]/div[2]/div[2]/div[1]/textarea')
-    story_text_div = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[1]/div[3]/div/div/div[1]/div[1]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[1]/div[1]/div/div')
-    
     sleep(10)
+
+
+def PlayAIDungeon(driver: webdriver.Chrome):
+    LoginToAIDungeon(driver)
+    submit_button = driver.find_element(By.XPATH, SUBMIT_BUTTON_XPATH)
+    text_area = driver.find_element(By.XPATH, TEXT_AREA_XPATH)
+    story_text_div = driver.find_element(By.XPATH, STORY_TEXT_DIV_XPATH)
+
     first_time = True
-    while(playing):
+    temp = ''
+    while (True):
         story_text = story_text_div.text
-        if(not first_time):
+        if (not first_time):
             story_text = story_text.replace(temp, '')
         GenerateImage(story_text)
         print(story_text)
-        temp = story_text
+        temp += story_text
         input_text = input()
         text_area.send_keys(input_text)
         submit_button.click()
@@ -60,4 +77,5 @@ def LaunchAIDungeon():
 
 
 GetApiKey()
-LaunchAIDungeon()
+driver = SetupDriver()
+PlayAIDungeon(driver)
